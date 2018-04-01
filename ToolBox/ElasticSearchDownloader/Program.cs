62b99d48d2a4caacb5d165ade69c73210b86bd7f
Jason H <jason.hillier@paviasystems.com>
2018-04-01 00:08:27 -0700

    es import

---
+++ ToolBox/ElasticSearchDownloader/Program.cs
@@ -0,0 +1,208 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Globalization;
+using QuantConnect.Configuration;
+using QuantConnect.Logging;
+using System.IO;
+using QuantConnect.Data.Market;
+using QuantConnect.Data;
+using System.Collections.Generic;
+using Nest;
+using System.Threading.Tasks;
+using System.Linq;
+using QuantConnect.ToolBox.IBConverter;
+
+namespace QuantConnect.ToolBox.ElasticSearchDownloader
+{
+	class Program
+	{
+		private static ElasticClient _client;
+		/// <summary>
+		/// QuantConnect Google Downloader For LEAN Algorithmic Trading Engine.
+		/// Original by @chrisdk2015, tidied by @jaredbroad
+		/// </summary>
+		public static void Main(string[] args)
+		{
+			if (args.Length < 4)
+			{
+				Console.WriteLine("Usage: ElasticSearchDownloader [symbol] [url] [index] [user] [pass]");
+				Environment.Exit(1);
+			}
+
+            //setup logging
+            Log.LogHandler = new CompositeLogHandler(new ILogHandler[] { new ConsoleLogHandler() });
+
+			Log.Trace("Downloading {0} quotes from {1}:{2}...", args);
+
+			_client = new Nest.ElasticClient(new ConnectionSettings(
+				new Uri(args[1]))
+				.DefaultIndex(args[2])
+				.BasicAuthentication(args[3], args[4])
+				.EnableHttpCompression()
+				);
+
+			//import the whole index
+			var quotes = FetchQuotes<StockOptionQuote>(args[0], DateTime.Now, DateTime.Now).Result;
+
+			importEquity(quotes);
+			importOptions(quotes);
+
+			Console.ReadLine();
+		}
+
+        private static void importEquity(IReadOnlyCollection<StockOptionQuote> quotes)
+        {
+			var symbol = quotes.First().baseSymbol;
+            TimeSpan timeSpan = new TimeSpan(0, 15, 0);
+            var symbolObject = Symbol.Create(symbol, SecurityType.Equity, Market.USA);
+
+			Log.Trace("Processing equity {0}...", symbol);
+
+			// Load settings from config.json
+			var dataDirectory = Config.Get("data-directory", "../../../Data");
+
+			var bars = new List<BaseData>();
+
+            foreach(var quote in quotes)
+            {
+                var bar = new TradeBar(
+                    quote.date,
+                    symbolObject,
+					quote.basePrice,
+					quote.basePrice,
+					quote.basePrice,
+					quote.basePrice,
+					0,
+                    timeSpan);
+
+                bars.Add(bar);
+            }
+
+			Log.Trace("Found {0} bars", bars.Count);
+
+            var writer = new LeanDataWriter(Resolution.Minute, symbolObject, dataDirectory);
+            writer.Write(bars);
+        }
+
+        private static void importOptions(IReadOnlyCollection<StockOptionQuote> quotes)
+        {
+			var underlying = quotes.First().baseSymbol;
+			TimeSpan timeSpan = new TimeSpan(0, 15, 0);
+			var underlyingSymbol = Symbol.Create(underlying, SecurityType.Equity, Market.USA);
+
+			Log.Trace("Processing options {0}...", underlying);
+
+			// Load settings from config.json
+			var dataDirectory = Config.Get("data-directory", "../../../Data");
+
+			
+            var bars = new List<BaseData>();
+			OptionDataWriter writer = null;
+			long ticksProcessed = 0;
+
+			foreach (var quote in quotes)
+			{
+                var optionSymbol = Symbol.CreateOption(
+                    underlying,
+                    Market.USA,
+                    OptionStyle.American,
+                    (quote.right == "P" ? OptionRight.Put : OptionRight.Call),
+                    (decimal)quote.strike,
+                    quote.date
+                );
+
+				//TODO: make a smart cache of this
+				if (writer == null ||
+					writer.ReferenceDate.Day != quote.date.Day ||
+					writer.Symbol.Value != optionSymbol.Value)
+				{
+					if (writer != null)
+					{
+						ticksProcessed += writer.SaveToDisk();
+						writer = null;
+					}
+					//yeah yeah this needs properly disposed of
+					writer = new OptionDataWriter(optionSymbol, quote.date, TickType.Quote, Resolution.Minute, dataDirectory);
+				}
+
+				var tick = new Tick(
+					quote.date,
+					optionSymbol,
+					(decimal)quote.mid,
+					(decimal)quote.bid,
+					(decimal)quote.ask
+					);
+
+				writer.Process(tick);
+            }
+
+			if (writer != null)
+			{
+				ticksProcessed += writer.SaveToDisk();
+				Log.Trace("Processed {0} ticks", ticksProcessed);
+				writer = null;
+			}
+        }
+
+
+		public static async Task<IReadOnlyCollection<T>> FetchQuotes<T>(string Symbol, DateTime Start, DateTime End) where T : StockOptionQuote, new()
+		{
+			List<T> documents = new List<T>();
+
+			var search = await _client.SearchAsync<T>(s => s
+				.AllTypes()
+				.From(0)
+				.Size(10000)
+				.Query(q => q
+					.Match(m => m
+						.Field(f=>f.symbol)
+						.Query(Symbol)
+					)/* && q
+					.DateRange(r => r
+						.Field(f => f.date)
+						.GreaterThanOrEquals(Start)
+						.LessThanOrEquals(End)
+					)*/
+				)
+				.Scroll(100000)
+				.Sort(ss=>ss.Ascending(f=>f.date))
+			);
+
+			Log.Trace("Found {0} quotes", search.HitsMetadata.Total);
+
+			//first page
+			string scrollId = search.ScrollId;
+			documents.AddRange(search.Documents);
+
+			ISearchResponse<T> results;
+			do
+			{
+				//page until we get all the results
+				results = await _client.ScrollAsync<T>(100000, scrollId);
+				scrollId = results.ScrollId;
+
+				documents.AddRange(results.Documents);
+
+				Log.Trace("Downloaded {0} so far", documents.Count);
+			} while (results.Documents.Count == 10000);
+
+			Log.Trace("Retrieved {0} document objects.", documents.Count);
+
+			return documents;
+		}
+	}
+}
---
+++ ToolBox/ElasticSearchDownloader/StockOptionQuote.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantConnect.ToolBox.ElasticSearchDownloader
+{
+	//BID_ASK quote
+	public class OptionQuote
+	{
+		public OptionQuote() { }
+
+		public string id { get; set; }
+		public string symbol { get; set; }
+		public string type { get; set; } //TODO: need to have normalized type names
+		public int contractId { get; set; }
+
+		public DateTime date { get; set; }
+		public DateTime expiry { get; set; }
+		public int daysExp { get; set; }
+
+		public double strike { get; set; }
+		public string right { get; set; }
+
+		public double bid { get; set; }
+
+		public double maxAsk { get; set; }
+
+		public double lowBid { get; set; }
+		public double ask { get; set; }
+		public long volume { get; set; }
+
+		public double spread
+		{
+			get { return Math.Round(ask - bid, 2); }
+		}
+		public double mid
+		{
+			get { return Math.Round(bid + (spread / 2), 2); }
+		}
+
+		public string baseSymbol { get; set; }
+		public string baseType { get; set; }
+	}
+
+	public class StockOptionQuote : OptionQuote
+	{
+		public StockOptionQuote() : base() { }
+
+		public decimal basePrice { get; set; }
+		public double dist { get; set; }
+
+		//In-the-money
+		public bool itm { get; set; }
+
+		public double intr { get; set; }
+
+		//extrinsic value
+		public double extr { get; set; }
+	}
+}

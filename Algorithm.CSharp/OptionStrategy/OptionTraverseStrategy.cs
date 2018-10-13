8cd984a3bafe85ee66950516f534b7d0aab1cbf3
jasonhillier <jason@hillier.us>
2018-10-13 15:03:22 -0700

    option traverse

---
+++ Algorithm.CSharp/OptionStrategy/OptionTraverseStrategy.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Orders;
+using QuantConnect.Securities.Option;
+using System.Collections.Generic;
+using QuantConnect.Securities;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    public class OptionTraverseStrategy
+    {
+        protected QCAlgorithm _Algo;
+        protected Option _Option;
+        protected Symbol _OptionSymbol;
+        protected int _AtmSpread;
+        protected int _MinDaysRemaining;
+        protected int _MaxDaysRemaining;
+
+        public OptionTraverseStrategy(QCAlgorithm Algo, Option Option, int AtmSpread = 2, int MinDaysRemaining = 2, int MaxDaysRemaining = 10)
+        {
+            this._Algo = Algo;
+            this._Option = Option;
+            this._OptionSymbol = Option.Symbol;
+            this._AtmSpread = AtmSpread;
+            this._MinDaysRemaining = MinDaysRemaining;
+            this._MaxDaysRemaining = MaxDaysRemaining;
+
+            this.OutsideOptionMinPrice = .20m;
+            this.OutsideOptionMinSpread = 2;
+            this.IsInvested = false;
+        }
+
+        public decimal OutsideOptionMinPrice { get; set; }
+        public int OutsideOptionMinSpread { get; set; }
+
+        public void Open(Slice slice, bool biasDown)
+        {
+            var chain = GetOptionChain(slice);
+            if (chain == null) return;
+            var inOptions = _SelectInsideOptions(slice, chain, biasDown);
+            if (inOptions == null) return;
+            var outOptions = _SelectOutsideOptions(slice, chain);
+            if (outOptions == null) return;
+
+            this.IsInvested = true;
+        }
+
+        public bool IsInvested { get; set; }
+
+        protected virtual Tuple<OptionContract,OptionContract> _SelectInsideOptions(Slice slice, OptionChain Chain, bool biasDown)
+        {
+            // we find nearby OTM contracts within target expiration
+            var otmContracts = Chain
+                .Where(x => biasDown ? x.Right == OptionRight.Put : x.Right == OptionRight.Call)
+                .Where(x => x.IsOTM(Chain))
+                .Where(x => (x.Expiry - x.Time).TotalDays >= _MinDaysRemaining)
+                .Where(x => (x.Expiry - x.Time).TotalDays <= _MaxDaysRemaining)
+                .OrderBy(x => x.Expiry)
+                .ThenBy(x => x.Strike - Chain.Underlying.Price).ToList();
+            if (otmContracts.Count > _AtmSpread)
+            {
+                //select ATM (to buy) and x away from ATM (to short)
+                var contractA = otmContracts.First();
+                var contractB = otmContracts.Take(_AtmSpread).First();
+
+                if (contractA != null &&
+                    contractB != null)
+                {
+                    //Buy A, Sell B
+                    return Tuple.Create(contractA, contractB);
+                }
+            }
+
+            Console.WriteLine("No valid INSIDE contracts!!");
+            return null;
+        }
+
+        protected virtual Tuple<OptionContract, OptionContract> _SelectOutsideOptions(Slice slice, OptionChain Chain)
+        {
+            // find farthest OTM PUT option
+            var otmContracts = Chain
+                .Where(x=>x.Right == OptionRight.Put)
+                .Where(x => x.IsOTM(Chain))
+                .Where(x => (x.Expiry - x.Time).TotalDays >= _MinDaysRemaining)
+                .Where(x => (x.Expiry - x.Time).TotalDays <= _MaxDaysRemaining)
+                .Where(x=> (x.AskPrice + x.BidPrice)/2 >= this.OutsideOptionMinPrice)
+                .Where(x=> x.Strike < Chain.Underlying.Price - this.OutsideOptionMinSpread)
+                .OrderBy(x => x.Expiry)
+                .ThenBy(x => x.Strike - Chain.Underlying.Price).ToList();
+            if (otmContracts.Count > 0)
+            {
+                var contractA = otmContracts.Last();
+
+                // find farthest OTM CALL option
+                otmContracts = Chain
+                    .Where(x => x.Right == OptionRight.Call)
+                    .Where(x => x.IsOTM(Chain))
+                    .Where(x => (x.Expiry - x.Time).TotalDays >= _MinDaysRemaining)
+                    .Where(x => (x.Expiry - x.Time).TotalDays <= _MaxDaysRemaining)
+                    .Where(x => (x.AskPrice + x.BidPrice) / 2 >= this.OutsideOptionMinPrice)
+                    .Where(x => x.Strike > Chain.Underlying.Price - this.OutsideOptionMinSpread)
+                    .OrderBy(x => x.Expiry)
+                    .ThenBy(x => x.Strike - Chain.Underlying.Price).ToList();
+                if (otmContracts.Count > 0)
+                {
+                    var contractB = otmContracts.Last();
+
+                    if (contractA != null &&
+                        contractB != null)
+                    {
+                        //SELL call AND put
+                        return Tuple.Create(contractA, contractB);
+                    }
+                }
+            }
+            Console.WriteLine("No valid OUTSIDE contracts!!");
+            return null;
+        }
+
+        public virtual OptionChain GetOptionChain(Slice slice)
+        {
+            OptionChain chain = null;
+            slice.OptionChains.TryGetValue(_OptionSymbol, out chain);
+            if (chain == null)
+            {
+                Console.WriteLine("<{0}> No option chain here!", slice.Time.ToString());
+                return null;
+            }
+
+            return chain;
+        }
+    }
+}
---
+++ Algorithm.CSharp/OptionsTraverseAlgo.cs
@@ -0,0 +1,215 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using QuantConnect.Indicators;
+using QuantConnect.Orders;
+using QuantConnect.Data.Consolidators;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    /// <summary>
+    /// This example demonstrates how to add options for a given underlying equity security.
+    /// It also shows how you can prefilter contracts easily based on strikes and expirations, and how you
+    /// can inspect the option chain to pick a specific option contract to trade.
+    /// </summary>
+    /// <meta name="tag" content="using data" />
+    /// <meta name="tag" content="options" />
+    /// <meta name="tag" content="filter selection" />
+    public class OptionsTraverseAlgo : QCAlgorithm, IRegressionAlgorithmDefinition
+    {
+        private const Resolution RESOLUTION = Resolution.Minute;
+		private const int MINUTE_RATE = 15;
+		private string UnderlyingTicker;
+        private int _ROC_THRESHOLD = 1;
+		public Symbol Underlying;
+		//public readonly Symbol OptionSymbol = QuantConnect.Symbol.Create(UnderlyingTicker, SecurityType.Option, Market.USA);
+        private OptionTraverseStrategy _Strategy;
+        private RateOfChangePercent _rocp;
+		private Slice _lastSlice;
+
+		private int _MinDaysRemaining = 10;
+        private int _AtmSpread = 3;
+		private int _RocPeriod = 9;
+
+		public override void Initialize()
+        {
+			UnderlyingTicker = GetParameter("symbol");
+			Underlying = QuantConnect.Symbol.Create(UnderlyingTicker, SecurityType.Equity, Market.USA);
+			//AAPL
+			//SetStartDate(2014, 06, 06);
+			//SetEndDate(2014, 06, 06);
+
+			DateTime startDate = DateTime.Parse(GetParameter("start-date"));
+			DateTime endDate = DateTime.Parse(GetParameter("end-date"));
+
+			_ROC_THRESHOLD = parseInt(GetParameter("roc"), _ROC_THRESHOLD);
+			_RocPeriod = parseInt(GetParameter("roc-period"), _RocPeriod);
+            /*
+			_MinDaysRemaining = parseInt(GetParameter("min-days"), _MinDaysRemaining);
+            _MinDaysRemaining = parseInt(GetParameter("max-days"), _MinDaysRemaining);
+            _AtmSpread = parseInt(GetParameter("atm-spread"), _AtmSpread);
+            */
+
+			//
+			SetStartDate(startDate);
+			SetEndDate(endDate);
+			//SetStartDate(2015, 01, 01);
+			//SetStartDate(2018, 02, 15);
+			//SetEndDate(2018, 03, 09);
+			SetCash(20000);
+
+			//this.
+
+            var equity = AddEquity(UnderlyingTicker, RESOLUTION);
+			var option = AddOption(UnderlyingTicker, RESOLUTION);
+
+            // use the underlying equity as the benchmark
+            SetBenchmark(equity.Symbol);
+
+			var consolidator = new TradeBarConsolidator(TimeSpan.FromMinutes(MINUTE_RATE));
+			consolidator.DataConsolidated += Consolidator_DataConsolidated;
+			_rocp = new RateOfChangePercent(_RocPeriod);
+			RegisterIndicator(Underlying, _rocp, consolidator);
+			SubscriptionManager.AddConsolidator(Underlying, consolidator);
+
+            // init strategy
+            _Strategy = new OptionTraverseStrategy(this, option);
+        }
+
+		private int parseInt(string val, int defaultValue)
+		{
+			int v;
+			if (!int.TryParse(val, out v))
+				v = defaultValue;
+			return v;
+		}
+
+		private decimal parseDec(string val, decimal defaultValue)
+		{
+			decimal v;
+			if (!decimal.TryParse(val, out v))
+				v = defaultValue;
+			return v;
+		}
+
+		private void Consolidator_DataConsolidated(object sender, TradeBar e)
+		{
+			if (IsMarketOpen(Underlying) && _lastSlice != null)
+			{
+				/*
+				if (this.Portfolio.TotalHoldingsValue == 0 &&
+					_rocp.Current.Value >= _ROC_THRESHOLD)
+				{
+					this.MarketOrder(Underlying, -100);
+					_quantity = -100;
+				}
+				else
+				{
+					if (this.Portfolio.TotalUnrealizedProfit > (this.Portfolio.TotalAbsoluteHoldingsCost * 0.05m))
+					{
+						this.Liquidate();
+					}
+					else if (this.Portfolio.TotalUnrealizedProfit < (this.Portfolio.TotalAbsoluteHoldingsCost * -0.05m))
+					{
+						_quantity *= 2;
+						this.MarketOrder(Underlying, _quantity);
+					}
+				}
+				*/
+
+				if (!_Strategy.IsInvested && _rocp.Current.Value*100 >= _ROC_THRESHOLD)
+				{
+					Console.WriteLine("<{0}>\tROCP= {1:00}", e.Time.ToString(), _rocp.Current.Value);
+
+                    _Strategy.Open(_lastSlice, true);
+				}
+				
+				/*else if (_Strategy.AggregateProfitPercentage(_lastSlice) < -.1m)
+				{
+					//if (_Strategy.AverageBasePrice(slice);
+					_Strategy.MarketBuyNextTierOptions(_lastSlice);
+				}
+				*/
+			}
+		}
+
+		/// <summary>
+		/// Event - v3.0 DATA EVENT HANDLER: (Pattern) Basic template for user to override for receiving all subscription data in a single event
+		/// </summary>
+		/// <param name="slice">The current slice of data keyed by symbol string</param>
+		public override void OnData(Slice slice)
+		{
+			_lastSlice = slice;
+
+			//Console.WriteLine("{0} Options: {1}", _lastSlice[Underlying].EndTime, _lastSlice.OptionChains.Count);
+		}
+
+		public override void OnEndOfAlgorithm()
+		{
+			//Print stats
+			Log("==== FINAL OPTION STRATEGY STATS ====");
+			//_Strategy.PrintStats();
+
+			base.OnEndOfAlgorithm();
+		}
+
+		/// <summary>
+		/// Order fill event handler. On an order fill update the resulting information is passed to this method.
+		/// </summary>
+		/// <param name="orderEvent">Order event details containing details of the evemts</param>
+		/// <remarks>This method can be called asynchronously and so should only be used by seasoned C# experts. Ensure you use proper locks on thread-unsafe objects</remarks>
+		public override void OnOrderEvent(OrderEvent orderEvent)
+        {
+            Log(orderEvent.ToString());
+        }
+
+        /// <summary>
+        /// This is used by the regression test system to indicate which languages this algorithm is written in.
+        /// </summary>
+        public Language[] Languages { get; } = { Language.CSharp, Language.Python };
+
+        /// <summary>
+        /// This is used by the regression test system to indicate what the expected statistics are from running the algorithm
+        /// </summary>
+        public Dictionary<string, string> ExpectedStatistics => new Dictionary<string, string>
+        {
+            {"Total Trades", "2"},
+            {"Average Win", "0%"},
+            {"Average Loss", "-0.28%"},
+            {"Compounding Annual Return", "-78.105%"},
+            {"Drawdown", "0.300%"},
+            {"Expectancy", "-1"},
+            {"Net Profit", "-0.280%"},
+            {"Sharpe Ratio", "0"},
+            {"Loss Rate", "100%"},
+            {"Win Rate", "0%"},
+            {"Profit-Loss Ratio", "0"},
+            {"Alpha", "0"},
+            {"Beta", "0"},
+            {"Annual Standard Deviation", "0"},
+            {"Annual Variance", "0"},
+            {"Information Ratio", "0"},
+            {"Tracking Error", "0"},
+            {"Treynor Ratio", "0"},
+            {"Total Fees", "$0.50"}
+        };
+    }
+}
